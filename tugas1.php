7afa16b2b177fa118bb1669429ee6a8a7d6c8760
muhammadaffant <80788855+muhammadaffant@users.noreply.github.com>
2022-03-03 18:02:49 +0700

    Delete tugas1.php

--- tugas1.php
+++
@@ -1,8 +0,0 @@
-C:\xampp\htdocs>php tugas1.php
-PHP Warning:  Module 'oci8' already loaded in Unknown on line 0
-
-Warning: Module 'oci8' already loaded in Unknown on line 0
-Nama : Muhammad Affan Tamami
-Nim : 20090151
-Kelas : 4A
-C:\xampp\htdocs>
